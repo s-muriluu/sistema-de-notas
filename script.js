8d34de9fbc50f5a39002ac664ee22c0f0ff30598
Sérgio Murilo dos Santos <sergiomurilosantos03@protonmail.com>
2023-11-11 18:09:29 -0300

    Implementado CRUD na seção de 'Alunos'. Agora, ao clicar no botão 'Alunos', são exibidos botões para realizar operações de criação, leitura, atualização e exclusão (CRUD). O formulário foi ajustado para tornar apenas o campo 'ID' editável ao carregar a página. Se houver um 'ID' preenchido, as informações correspondentes são recuperadas do localStorage, preenchendo todos os campos. Com um aluno selecionado, é possível excluí-lo clicando no botão 'Excluir Aluno'. Ao clicar em 'Criar Aluno', os campos 'Nome' e 'Turma' ficam editáveis, permitindo a inclusão de um novo aluno com todas as informações preenchidas.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class Turma {
-    constructor(id, num, ano){
+    constructor(id, codigo, ano){
         this.id = id,
-        this.num = num,
+        this.codigo = codigo,
         this.ano = ano
     }
 }
@@ -18,7 +18,6 @@ let dadosAlunos = localStorage.getItem("Alunos");
 let alunos = dadosAlunos ? JSON.parse(dadosAlunos) : []
 let dadosTurmas = localStorage.getItem("Turmas");
 let turmas = dadosTurmas ? JSON.parse(dadosTurmas) : []
-
 let content = document.getElementById("content");
 
 function wipeData(){
@@ -28,8 +27,9 @@ function wipeData(){
 }
 
 function btnAlunos() {
-    cadastrarAluno();
+    formAlunos();
     pesquisarAlunos();
+    document.getElementById("idAluno").focus();
 }
 
 function btnTurmas() {
@@ -41,14 +41,61 @@ function btnNotas() {
     //
 }
 
-function cadastrarAluno(){
+function atualizacaoAluno(){
+    if (this.idAluno.value == ""){
+        document.getElementById("nomeAluno").disabled = true;
+        document.getElementById("turmaAluno").disabled = true;
+        document.getElementById("btnAtualizarAluno").disabled = true;
+        document.getElementById("btnExcluirAluno").disabled = true;
+        this.nomeAluno.value = "";
+        this.turmaAluno.value = "";
+    } else {
+        document.getElementById("nomeAluno").disabled = true;
+        document.getElementById("turmaAluno").disabled = true;
+        document.getElementById("btnAtualizarAluno").disabled = false;
+        document.getElementById("btnExcluirAluno").disabled = false;
+        for (let cont=0; cont<alunos.length; cont++){
+            if (this.idAluno.value == alunos[cont].id) {
+                this.nomeAluno.value = alunos[cont].nome;
+                this.turmaAluno.value = alunos[cont].turma;
+            }
+        }
+    }
+}
+
+function cancelar(){
+    document.getElementById("nomeAluno").disabled = true;
+    document.getElementById("turmaAluno").disabled = true;
+    document.getElementById("btnAtualizarAluno").disabled = true;
+    document.getElementById("btnExcluirAluno").disabled = true;
+    btnAlunos();
+}
+
+function criarAluno() {
+    btnAlunos();
+    document.getElementById("idAluno").disabled = true;
+    document.getElementById("nomeAluno").disabled = false;
+    document.getElementById("turmaAluno").disabled = false;
+    document.getElementById("btnCriarAluno").disabled = true;
+    document.getElementById("btnAdicionarAluno").disabled = false;
+    document.getElementById("btnCancelar").disabled = false;
+    document.getElementById("nomeAluno").focus();
+}
+
+function formAlunos(){
     content.innerHTML = `<div id="formulario">
     <form>
-        <label for="nome">Nome</label><br>
-        <input type="text" name="nome" id="nome"><br>
-        <label for="turma">Turma</label><br>
-        <input type="text" name="turma" id="turma"><br><br>
-        <button onclick="addAluno()">Adicionar Aluno</button>
+        <label for="idAluno">ID</label><br>
+        <input type="text" name="idAluno" id="idAluno" oninput="atualizacaoAluno()"><br>
+        <label for="nomeAluno">Nome</label><br>
+        <input type="text" name="nomeAluno" id="nomeAluno" disabled><br>
+        <label for="turmaAluno">Turma</label><br>
+        <input type="text" name="turmaAluno" id="turmaAluno" disabled><br><br>
+        <button onclick="criarAluno()" id="btnCriarAluno">Criar Aluno</button>
+        <button onclick="addAluno()" id="btnAdicionarAluno" disabled>Adicionar Aluno</button>
+        <button onclick="" id="btnAtualizarAluno" disabled>Atualizar Aluno</button>
+        <button onclick="excluirAluno()" id="btnExcluirAluno" disabled>Excluir Aluno</button>
+        <button onclick="cancelar()" id="btnCancelar" disabled>Cancelar</button>
     </form>
 </div>`
 }
@@ -97,13 +144,23 @@ function pesquisarTurmas() {
         let cell2 = row.insertCell(1);
         let cell3 = row.insertCell(2);
         cell1.innerHTML = turmas[cont].id;
-        cell2.innerHTML = turmas[cont].num;
+        cell2.innerHTML = turmas[cont].codigo;
         cell3.innerHTML = turmas[cont].ano;
     }
 }
 
+function excluirAluno() {
+    for (let cont=0; cont<alunos.length; cont++){
+        if (this.idAluno.value == alunos[cont].id) {
+            alunos.splice(cont, 1);
+        }
+    }
+    localStorage.setItem("Alunos", JSON.stringify(alunos));
+    btnAlunos();
+}
+
 function addAluno() {
-    let aluno = new Aluno(alunos.length, this.nome.value, this.turma.value);
+    let aluno = new Aluno(alunos[alunos.length-1].id+1, this.nomeAluno.value, this.turmaAluno.value);
     alunos.push(aluno);
     localStorage.setItem("Alunos", JSON.stringify(alunos));
     btnAlunos();
