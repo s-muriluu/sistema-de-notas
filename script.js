c6b545b87b25067703ec57936c540813b5c35df4
Sérgio Murilo dos Santos <sergiomurilosantos03@protonmail.com>
2023-11-11 16:03:06 -0300

    Adicionado interface para o sistema com menu que levará as funcionalidades de cadastro de alunos, turmas e notas. Função de adicionar aluno e pesquisa de alunos e turmas já foram desenvolvidas.

--- script.js
+++ script.js
@@ -1,154 +1,116 @@
-/*
-Sistema para treinar logica de programacao no JS
-Utilizado console para saida de dados e prompt para entrada
-Funcionalidades:
-- Cadastro de alunos
-- Cadastro de notas
-  -> serao armazenados 4 notas
-- Listagem dos alunos e medias
-*/
+class Turma {
+    constructor(id, num, ano){
+        this.id = id,
+        this.num = num,
+        this.ano = ano
+    }
+}
 
 class Aluno {
-    constructor(nome) {
-        // propriedades nome aluno
+    constructor(id, nome, turma){
+        this.id = id,
         this.nome = nome,
-        // propriedade notas seras
-        this.notas = [],
-        // soma das notas
-        this.total = 0,
-
-        // metodo para calcular media aluno
-        this.media = function() {
-            this.notas.forEach(nota => {
-                // soma todas as notas
-                this.total += nota;
-            });
-            // retorna a media
-            return this.total / 4;
-        };
+        this.turma = turma
     }
 }
 
-function menu(){
-    return prompt(`
----------------------------
-[ 1 ] Cadastro aluno
-[ 2 ] Cadastro notas
-[ 3 ] Media alunos
-[ 0 ] Sair
----------------------------
-Escolha uma opcao do menu:`);
+let dadosAlunos = localStorage.getItem("Alunos");
+let alunos = dadosAlunos ? JSON.parse(dadosAlunos) : []
+let dadosTurmas = localStorage.getItem("Turmas");
+let turmas = dadosTurmas ? JSON.parse(dadosTurmas) : []
+
+let content = document.getElementById("content");
+
+function wipeData(){
+    localStorage.clear();
+    alunos = []
+    turmas = []
 }
 
-function criarAluno() {
-    let aluno = new Aluno(prompt('Digite o nome do aluno:'));
-    console.log(`Aluno ${aluno.nome} cadastrado`);
-    return aluno;
+function btnAlunos() {
+    cadastrarAluno();
+    pesquisarAlunos();
 }
 
-function listAlunos(){
-    console.log(`Lista de alunos
----------------------------
-Cod - Nome
----------------------------`);
-    for (let aluno of alunos.entries()){
-        console.log(`${aluno[0]} - ${aluno[1].nome}`);
-    }
-    console.log('---------------------------');
+function btnTurmas() {
+    cadastrarTurmas();
+    pesquisarTurmas();
 }
 
-// listagem dos nomes dos alunos e medias
-function listMedias(){
-    console.log(`---------------------------
-Nome - Media`);
-    alunos.forEach(aluno =>{
-        // ira verificar se o aluno tem notas lancadas
-        if (aluno.notas.length != 0){
-            console.log(`${aluno.nome} - ${aluno.media()}`);
-        } else {
-            console.log(`${aluno.nome} - Notas nao cadastradas`);
-        }
-    })
+function btnNotas() {
+    //
 }
 
-function cadastrarNotas(cod){
-    console.log(`Notas do aluno ${alunos[cod].nome}`);
-    let nota;
-    for (let cont = 1; cont <= 4; cont++){
-        nota = parseFloat(prompt(`Nota ${cont}`));
-        console.log(`Nota ${cont} = ${nota}`);
-        // verifica se nota digitada esta entre 0 a 10
-        if (nota < 0 || nota > 10){
-            console.log('Nota invalida! Digite novamente.')
-            cont--;
-        } else {
-            alunos[cod].notas.push(nota);
-        }
-    }
+function cadastrarAluno(){
+    content.innerHTML = `<div id="formulario">
+    <form>
+        <label for="nome">Nome</label><br>
+        <input type="text" name="nome" id="nome"><br>
+        <label for="turma">Turma</label><br>
+        <input type="text" name="turma" id="turma"><br><br>
+        <button onclick="addAluno()">Adicionar Aluno</button>
+    </form>
+</div>`
 }
 
-// verifica se ja foram lancadas notas para o aluno
-function varreduraNotas(cod){
-    if (alunos[cod].notas.length == 0) {
-        // nenhuma nota lancada
-        return false;
-    } else {
-        // notas ja lancadas
-        return true;
+function cadastrarTurmas(){
+    content.innerHTML = `<div id="formulario">
+    </div>`
+}
+
+function pesquisarAlunos() {
+    content.innerHTML += `<div id="pesquisa">
+    <table id="tabela">
+    <tr>
+    <th>ID</th>
+    <th>NOME</th>
+    <th>TURMA</th>
+    </tr>
+    </table>
+    </div>`
+    let tabela = document.getElementById("tabela");
+    for (let cont=0; cont < alunos.length; cont++) {
+        let row = tabela.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = alunos[cont].id;
+        cell2.innerHTML = alunos[cont].nome;
+        cell3.innerHTML = alunos[cont].turma;
     }
 }
 
-// var para armazenar todos os alunos
-var alunos = new Array();
-// var para navegar pelo menu
-var opcao = null;
-
-// main loop
-while (opcao != 0){
-    // chamada funcao menu, escolha do usuario armazenada na var opcao
-    opcao = menu();
-    switch (opcao){
-        case '1':
-            // Cadastro aluno
-            alunos.push(criarAluno());
-            break;
-        case '2':
-            // Cadastro notas
-            // se houver alunos cadastrados ira listar
-            if (alunos.length > 0){
-                listAlunos();
-                aluno = prompt('Escolha um aluno para dar nota:');
-                if (aluno > alunos.length-1){
-                    console.log('Opcao invalida!');
-                    break;
-                } else if (varreduraNotas(aluno)) {
-                    // chamada varredura
-                    // return true
-                    console.log('Notas do aluno ' + alunos[aluno] + ' ja foram cadastradas');
-                    break;
-                } else {
-                    // chamada funcao para cadastro
-                    // return false
-                    cadastrarNotas(aluno);
-                }
-            } else {
-                // mensagem quando nao ha alunos cadastrados
-                console.log('Nao ha alunos cadastrados!')
-            }
-            break;
-        case '3':
-            // Media alunos
-            console.log('Listando medias:');
-            listMedias();
-            break;
-        case '0':
-            // Sair
-            console.log('Volte sempre!');
-            break;
-        default:
-            // Tratando excecoes
-            console.log('Opcao invalida!');
-            break;
+function pesquisarTurmas() {
+    content.innerHTML += `<div id="pesquisa">
+    <table id="tabela">
+    <tr>
+    <th>ID</th>
+    <th>NUMERO</th>
+    <th>ANO</th>
+    </tr>
+    </table>
+    </div>`
+    let tabela = document.getElementById("tabela");
+    for (let cont=0; cont < turmas.length; cont++) {
+        let row = tabela.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = turmas[cont].id;
+        cell2.innerHTML = turmas[cont].num;
+        cell3.innerHTML = turmas[cont].ano;
     }
-    console.log('---------------------------');
+}
+
+function addAluno() {
+    let aluno = new Aluno(alunos.length, this.nome.value, this.turma.value);
+    alunos.push(aluno);
+    localStorage.setItem("Alunos", JSON.stringify(alunos));
+    btnAlunos();
+}
+
+function addTurmas() {
+    let turma = new Turma(3, 2, 1);
+    turmas.push(turma);
+    localStorage.setItem("Turmas", JSON.stringify(turmas));
 }
